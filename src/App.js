017cef7c8524f52ed7adf242deee4afb8e44006c
Daniel <mikyjo2012@gmail.com>
2023-05-26 02:42:02 -0500

    using cat api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import cardImg from './image.jpg'
+
 import React, { useState, useEffect } from "react";
 import Overview from "./components/Overview";
 
+function Card(url) {
+  this.url = url
+}
 function App() {
-  const [cardArray, setCardArray] = useState([]);
-  const [card, setCard] = useState('');
-  useEffect(() => {
-    setCard('example');
-    setCardArray(['fdsa']);
-    console.log(cardArray);
-  }, []);
+  const card = new Card('../image.jpg')
+  const [img, setImg] = useState();
 
+  
+  const [cardArray, setCardArray] = useState([]);
+
+
+  useEffect(() => {
+    setCardArray(cardArray.concat(card));
+
+    fetch('https://api.thecatapi.com/v1/images/search?limit=10&api_key=live_t5C5EKXsYxtt6GJwSaNDJRU487gxMPUeNHyjaXw8BQa6fCmbT96XJG4JmBTmKguC', {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+      
+    }).then(function(response) {
+      setImg(response[0].url);
+ 
+    });;
+  }, []);
+  
 
   const incrementCount = () => {
     console.log("lol");
   };
   return (
     <div className="App">
-      {/* <Overview cards={cardArray} /> */}
+       <Overview cards={cardArray} /> 
+       <img src={img}></img>
       <div onClick={incrementCount}>
-        <img src={cardImg}/>
+      
         
       </div>
     </div>
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,13 +1,16 @@
 // Overview.js
 
 import React from "react";
+import logo from './image.jpg';
 
 function Overview(props) {
     const { cards } = props;
     return (
         <div>
             {cards.map((card) => {
-                return <div>XDDDD</div>;
+                return <div>
+                    <img src={logo}></img>
+                    </div>;
               })}
         </div>
       );
