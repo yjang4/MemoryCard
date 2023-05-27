973789f45fa2fdb1f61c07996d8089b6b41dcb18
Daniel <mikyjo2012@gmail.com>
2023-05-26 22:38:04 -0500

    cool url

--- src/App.js
+++ src/App.js
@@ -6,20 +6,27 @@ import Overview from "./components/Overview";
 function App() {
 
   const [api, setApi] = useState();
-  const [img, setImg] = useState();
+  const [img, setImg] = useState([]);
 
   useEffect(() => {
     async function getData() {
-      const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=g72i3yeiL9kBl8o5DmSq0Dr9I3MSa2Vw&s=cat', {mode: 'cors'})
+      const response = await fetch('https://danbooru.donmai.us/posts.json?d=1&tags=order%3Arank', {mode: 'cors'})
       const catData = await response.json();
-      setImg(catData.data.images.original.url);
+      console.log(catData);
+      //setImg(img.concat(catData.data.images.original.url));
+
     }
+
     getData()
+    
+    
   }, []);
 
   return (
     <div className="App">
-      <img src={img}></img>
+      {/* <img src={img}></img>
+
+      {console.log(img)} */}
       
         
 
