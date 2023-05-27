216b37cf0c15046e45acbe19d6bf9cc9ad73f887
Daniel <mikyjo2012@gmail.com>
2023-05-26 21:14:39 -0500

    fetch and display image

--- src/App.js
+++ src/App.js
@@ -2,39 +2,24 @@
 import React, { useState, useEffect } from "react";
 import Overview from "./components/Overview";
 
-function Card(url) {
-  this.url = url
-}
+
 function App() {
-  const card = new Card('../image.jpg')
-  const [img, setImg] = useState();
+
   const [api, setApi] = useState();
-
-  
-  const [cardArray, setCardArray] = useState([]);
-
+  const [img, setImg] = useState();
 
   useEffect(() => {
-    setCardArray(cardArray.concat(card));
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=beng&api_key=live_t5C5EKXsYxtt6GJwSaNDJRU487gxMPUeNHyjaXw8BQa6fCmbT96XJG4JmBTmKguC`, {mode: 'cors'})
-    .then((response) => response.json())
-    .then((actualData) => setApi(actualData));
+    async function getData() {
+      const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=g72i3yeiL9kBl8o5DmSq0Dr9I3MSa2Vw&s=cat', {mode: 'cors'})
+      const catData = await response.json();
+      setImg(catData.data.images.original.url);
+    }
+    getData()
   }, []);
-  
-  console.log(cardArray);
 
-  const incrementCount = () => {
-    console.log("lol");
-  };
   return (
     <div className="App">
-      {/*  <Overview cards={cardArray} />  */}
-       {/* <img src={img}></img>  */}
-       {api &&
-          api.map(({ url }) => (
-            <img src={url}/>
-          ))}
-      {/* <div onClick={incrementCount}/> */}
+      <img src={img}></img>
       
         
 
