7e9003750f5f8e636e455231fff3d82ed02ddc21
Daniel <mikyjo2012@gmail.com>
2023-05-27 22:31:16 -0500

    load images

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-/* https://api.giphy.com/v1/gifs/translate?api_key=g72i3yeiL9kBl8o5DmSq0Dr9I3MSa2Vw&s=cats */
 import React, { useState, useEffect } from "react";
-import Overview from "./components/Overview";
-
+import CardDisplay from "./components/CardDisplay";
 
 function App() {
 
-  const [api, setApi] = useState();
   const [img, setImg] = useState([]);
 
   useEffect(() => {
     async function getData() {
-      const response = await fetch('https://danbooru.donmai.us/posts.json?d=1&tags=order%3Arank', {mode: 'cors'})
-      const catData = await response.json();
-      console.log(catData);
-      //setImg(img.concat(catData.data.images.original.url));
-
-    }
-
-    getData()
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100', {mode: 'cors'})
+      const pokeData = await response.json();
+      for (let i = 0; i < pokeData.results.length; i ++) {
+        const newUrl = pokeData.results[i].url;
+        const response2 = await fetch(newUrl, {mode: 'cors'});
+        const pokeData2 = await response2.json();
+        const spriteUrl = pokeData2.sprites.front_shiny;
+        setImg(img => ([...img, spriteUrl]));
+        
+      } 
     
+    }
+    getData()
     
   }, []);
 
   return (
     <div className="App">
-      {/* <img src={img}></img>
-
-      {console.log(img)} */}
-      
-        
+      <CardDisplay cards={img}/>
 
     </div>
   );
---
+++ src/components/CardDisplay.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function CardDisplay(props) {
+    const { cards } = props;
+    const random = Math.floor(Math.random() * 100);
+
+    return (
+        <div>
+            {/* {cards.map((card) => {
+                return <div>
+                    <img src={card}></img>
+                    </div>;
+              })} */}
+              <img src={cards[random]}></img>
+        </div>
+      );
+}
+export default CardDisplay;
